0fdbecfc1922423fa2c9d5c5c35e09270352d1b3
Adam Holt <adamholt@Hoth.(none)>
2010-03-11 22:51:04 +0000

    0.0.1, nowhere near ready for primetime

---
+++ Rakefile
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'rake'
+ 
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gem|
+    gem.name = "rackson-five"
+    gem.summary = %Q{Add some Jackson 5 love to your rack based website. It's as easy as 1-2-3}
+    gem.description = %Q{Embeds Jackson Five members into your rack based application}
+    gem.email = "ads.noob@gmail.com"
+    gem.homepage = "http://github.com/omgitsads/rackson-five"
+    gem.files = FileList["[A-Z]*","lib/**/*"]
+    gem.authors = ["Adam Holt"]
+  end
+  Jeweler::GemcutterTasks.new
+rescue LoadError
+  puts "Jeweler (or a dependency) not available. Install it with: sudo gem install jeweler"
+end
---
+++ lib/rack/rackson-five.rb
@@ -0,0 +1,43 @@
+module Rack
+  class RacksonFive < Struct.new :options
+    def initialize(app)
+      @app = app
+    end
+    
+    def call(env)
+      request = Rack::Request.new(env)
+      if request.path_info =~ /^\/rackson-five\/(.+)$/
+        rackson_five_request($1)
+      else
+        jacksonify(env)
+      end
+    end
+    
+    def rackson_five_request(path)
+      if path == "rackson-five.png"
+        data = ::File.open(::File.join(::File.dirname(__FILE__),"images","group.png"),"r").read
+        [200, {"Content-Type" => "image/png"},data]
+      end
+    end
+    
+    def jacksonify(env)
+      status, headers, response = @app.call(env)
+      if headers["Content-Type"] =~ /text\/html|application\/xhtml\+xml/
+        body = ""
+        response.each { |part| body << part }
+        index = body.rindex("</body>")
+        if index
+          body.insert(index, '<img src="/rackson-five/rackson-five.png" />')
+          headers["Content-Length"] = body.length.to_s
+          response = [body]
+        end
+      end
+      [status, headers, response]
+    end
+    
+    def member
+      all_members = [:group, :micheal, :tito, :jermaine, :randy, :marlon, :jackie]
+      available_members = all_members - (all_members - options[:members])
+    end
+  end
+end
